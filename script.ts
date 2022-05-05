8870ba03115ae5f3e91472987e2d50390608604b
RSacerdote <rodrigo.sacerdote@hotmail.com>
2022-05-05 15:32:06 -0300

    transition to TypeScript

--- script.ts
+++ script.ts
@@ -1,4 +1,3 @@
-"use strict";
 const X_CLASS = 'x';
 const O_CLASS = 'o';
 const WINNING_COMBINATIONS = [
@@ -11,48 +10,53 @@ const WINNING_COMBINATIONS = [
     [0, 4, 8],
     [6, 4, 2]
 ];
-const cellElements = document.querySelectorAll('[data-cell]');
+const cellElements: NodeListOf<HTMLElement> = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 const menu = document.getElementById('Menu');
-const menuMessageElement = document.querySelector('[data-endgame-messsage-text]');
+const menuMessageElement: HTMLElement | null = document.querySelector('[data-endgame-messsage-text]');
 const restartButton = document.getElementById('restartButton');
-let currentClass;
+let currentClass: string;
+
 startGame();
-restartButton === null || restartButton === void 0 ? void 0 : restartButton.addEventListener('click', startGame);
-function startGame() {
+restartButton?.addEventListener('click', startGame);
+
+function startGame(): void {
     currentClass = X_CLASS;
-    console.log("a");
+    console.log("a")
     cellElements.forEach(cell => {
-        console.log("9");
+        console.log("9")
         cell.classList.remove(X_CLASS);
         cell.classList.remove(O_CLASS);
         cell.removeEventListener('click', handleCLick);
-        cell.addEventListener('click', handleCLick, { once: true });
-    });
+        cell.addEventListener('click', handleCLick, {once: true});
+    })
     setBoardHoverClass();
-    menu === null || menu === void 0 ? void 0 : menu.classList.remove('show');
+    menu?.classList.remove('show');
 }
-function handleCLick(e) {
-    console.log("clicked");
+
+function handleCLick(e: Event): void {
+    console.log("clicked")
     const cell = e.target;
     if ((cell !== null) && (cell instanceof HTMLElement)) {
         placeMark(cell);
     }
-    if (checkWin()) {
+    if (checkWin()){
         endGame(false);
-    }
-    else if (isDraw()) {
+    } 
+    else if (isDraw()){
         endGame(true);
-    }
+    } 
     else {
         currentClass = (currentClass == X_CLASS) ? O_CLASS : X_CLASS;
         setBoardHoverClass();
     }
 }
-function placeMark(cell) {
+
+function placeMark(cell: HTMLElement): void{
     cell.classList.add(currentClass);
 }
-function endGame(draw) {
+
+function endGame(draw: boolean): void{
     if (menuMessageElement !== null) {
         if (draw) {
             menuMessageElement.innerText = 'Draw!';
@@ -61,22 +65,25 @@ function endGame(draw) {
             menuMessageElement.innerText = `${currentClass.toUpperCase()} won!`;
         }
     }
-    menu === null || menu === void 0 ? void 0 : menu.classList.add('show');
+    menu?.classList.add('show');
 }
-function setBoardHoverClass() {
-    board === null || board === void 0 ? void 0 : board.classList.remove(X_CLASS);
-    board === null || board === void 0 ? void 0 : board.classList.remove(O_CLASS);
-    board === null || board === void 0 ? void 0 : board.classList.add(currentClass);
+
+function setBoardHoverClass(): void {
+    board?.classList.remove(X_CLASS);
+    board?.classList.remove(O_CLASS);
+    board?.classList.add(currentClass)
 }
-function isDraw() {
+
+function isDraw(): boolean {
     return [...cellElements].every(cell => {
         return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
-    });
+    })
 }
-function checkWin() {
+
+function checkWin(): boolean {
     return WINNING_COMBINATIONS.some(combination => {
         return combination.every(index => {
-            return cellElements[index].classList.contains(currentClass);
-        });
-    });
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
 }
